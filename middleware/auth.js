ad301e136f50e276dcdecdf01383b59c3cce4dc7
fouadej <eljilalifouad@gmail.com>
2021-12-14 23:00:18 +0100

    last commit

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+//-------------- Authentifiaction --------------
+
+const jwt = require('jsonwebtoken');
+
+
+module.exports = (req, res, next) => {
+    try{
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, process.env.RANDOM_SECRET_KEY);
+        const userId = decodedToken.userId;
+        req.auth = { userId };
+        // -------- vérifier l'identité de l'utilisateur
+        if(req.body.userId && req.body.userId !== userId) {
+            throw 'User Id non valide !';
+        } else {
+            next();
+        }
+    } catch (error) {
+        res.status(403).json({ error: error | 'unauthorized request !' });
+    }
+};
